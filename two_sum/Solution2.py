53e5bcee0b054b1708e4f96c57e93e659910ae1b
haarlemmer <42113414+haarlemmer@users.noreply.github.com>
2018-12-29 19:19:38 +0800

    Update Solution2.py

--- two_sum/Solution2.py
+++ two_sum/Solution2.py
@@ -1,10 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 class Solution2:
     def twoSum(self, nums, target):
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 sum = nums[i] + nums[j]
                 if sum == target:
-                    return(i, j)
+                    return [i, j]
 nums = [2,7,9,11]
 target = 9
-print(twoSum(nums,target))
+print(Solution2.twoSum("",nums,target))
